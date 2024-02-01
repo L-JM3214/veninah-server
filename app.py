041b7da91b5dd30dcca4a9a18d508abffb237e79
L-JM3214 <lactorjm4@gmail.com>
2024-02-02 02:48:54 +0300

    Mpesa Route Method

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ with app.app_context():
         print(inspector.get_table_names())
         
 
-
 #Mpesa
 consumer_key='7GSlEmZiocYKga9acUBDyIYiuJqOvZvHd6XGzbcVZadPm93f'
 consumer_secret='Vh2mvQS4GKyo6seUtpAApN1plTwMDTeqyGZNBEtESYH05sBfRMSddn5vnlJ4zifA'
@@ -71,7 +70,7 @@ def get_foods():
     return response
 
 #register users
-@app.route('/users', methods=['POST'])
+@app.route('/user', methods=['POST'])
 def register_user():
     data = request.get_json()
 
@@ -226,82 +225,73 @@ def get_addresses_by_user(user_email):
     else:
         return make_response(jsonify({"error": "User not found"}), 404)
 
-@app.route('/payment')
+@app.route('/payment', methods=['POST', 'OPTIONS'])
 def post():
+    if request.method == 'OPTIONS':
+        # Pre-flight request
+        response = make_response('', 200)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
 
-        # phone_number = request.json['phone']
-        # amount = request.json['amount']
-        parser = reqparse.RequestParser()
-        parser.add_argument('phone', type=str, required = True)
-        parser.add_argument('amount', type=str, required=True)
-        args = parser.parse_args()
+    # The rest of your payment processing logic here
+    parser = reqparse.RequestParser()
+    parser.add_argument('phone', type=str, required=True)
+    parser.add_argument('amount', type=str, required=True)
+    args = parser.parse_args()
 
-        phone_number = args['phone']
-        amount = args['amount']
+    phone_number = args['phone']
+    amount = args['amount']
 
-        consumer_key ="AAa4RjX5YgWolpQsX8b1E6MAZDHH1zRXpfXBWnjfGSWImQEU"
-        consumer_secret = "pPAPZ4X3uvGyfeFpEoziaxR43lRih7PxnHV2FA62sCOgmWwKAnZ5S6sdIlhRwXlf"
-        api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    consumer_key = "AAa4RjX5YgWolpQsX8b1E6MAZDHH1zRXpfXBWnjfGSWImQEU"
+    consumer_secret = "pPAPZ4X3uvGyfeFpEoziaxR43lRih7PxnHV2FA62sCOgmWwKAnZ5S6sdIlhRwXlf"
+    api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
-        r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 
-        data = r.json()
-        access_token = "Bearer " + data['access_token']
-        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-        bussiness_shortcode = '174379'
-        data_to_encode = bussiness_shortcode + passkey + timestamp
-        encoded_data = base64.b64encode(data_to_encode.encode())
-        password = encoded_data.decode('utf-8')
+    data = r.json()
+    access_token = "Bearer " + data['access_token']
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+    bussiness_shortcode = '174379'
+    data_to_encode = bussiness_shortcode + passkey + timestamp
+    encoded_data = base64.b64encode(data_to_encode.encode())
+    password = encoded_data.decode('utf-8')
 
-        request = {
-            "BusinessShortCode": bussiness_shortcode,
-            "Password": password,
-            "Timestamp": timestamp, # timestamp format: 20190317202903 yyyyMMhhmmss 
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": amount,
-            "PartyA": f"254{phone_number[1:]}",
-            "PartyB": bussiness_shortcode,
-            "PhoneNumber": f"254{phone_number[1:]}",
-            "CallBackURL": "https://mydomain.com/pat",
-            "AccountReference": "Client",
-            "TransactionDesc": "Client Paid"
-        }
+    request_payload = {
+        "BusinessShortCode": bussiness_shortcode,
+        "Password": password,
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        "PartyA": f"254{phone_number[1:]}",
+        "PartyB": bussiness_shortcode,
+        "PhoneNumber": f"254{phone_number[1:]}",
+        "CallBackURL": "https://mydomain.com/pat",
+        "AccountReference": "Client",
+        "TransactionDesc": "Client Paid"
+    }
 
-        stk_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    stk_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": access_token, "Content-Type": "application/json"}
 
-        headers = {"Authorization": access_token, "Content-Type": "application/json"}
+    response = requests.post(stk_url, json=request_payload, headers=headers)
 
-        # STK push
+    if response.status_code > 299:
+        mpesa_response = {'message': 'Failed'}
+        final_response = make_response(jsonify(mpesa_response))
+        return final_response
+    else:
+        message = {'message': 'Successful'}
+        response = make_response(jsonify(message))
 
-        response = requests.post(stk_url,json=request,headers=headers)
+        # Assuming you have a 'Payment' model, you can add the payment data to the database here
+        new_data = Payment(number=phone_number, amount=amount)
+        db.session.add(new_data)
+        db.session.commit()
 
-        if response.status_code > 299:
-            mpesa_response = {
-                'message':'Failed'
-            }
-            final_response = make_response(
-                jsonify(mpesa_response)
-            )
-
-            return final_response
-        else:
-            message = {
-                'message':'Successful'
-            }
-            response = make_response(
-                jsonify(message)
-            )
-
-            new_data = Payment(
-                number = phone_number,
-                amount = amount
-            )
-
-            db.session.add(new_data)
-            db.session.commit()
-
-            return response
+        return response
 
 
 @app.route('/send_confirmation', methods=['POST'])
@@ -391,6 +381,52 @@ def send_confirmation():
     except Exception as e:
         print(f'Error: {str(e)}')  
         return jsonify({'success': False, 'message': f'Error: {str(e)}'})
+        
+
+@app.route('/locations', methods=['GET'])
+def get_locations():
+    locations = []
+    for location in Location.query.all():
+        response_body = {
+            "id": location.id,
+            "name": location.name,
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "delivery_fee": location.delivery_fee
+        }
+        locations.append(response_body)
+    response = make_response(
+        jsonify(locations),
+        200
+    )
+    return response
+
+
+@app.route('/locations/<int:id>', methods=['GET'])
+def get_location(id):
+    location = Location.query.get(id)
+    if location is None:
+        return make_response(jsonify({"message": "Location not found"}), 404)
+    response_body = {
+        "id": location.id,
+        "name": location.name,
+        "latitude": location.latitude,
+        "longitude": location.longitude
+    }
+    return make_response(jsonify(response_body), 200)
+
+
+# Get distance from google maps API
+@app.route('/distance', methods=['GET'])
+def get_distance():
+    origin = request.args.get('origins')
+    destination = request.args.get('destinations')
+    api_key = request.args.get('key')
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={}&destinations={}&key={}".format(
+        origin, destination, api_key)
+    response = requests.get(url)
+    print(response.json())
+    return response.json()
 
     
 
