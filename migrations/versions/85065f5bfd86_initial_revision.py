20a4b46f9598502fcd18f353efcbb2d2a7836ee7
L-JM3214 <lactorjm4@gmail.com>
2024-02-05 17:20:18 +0300

    Deployed Redner

---
+++ migrations/versions/85065f5bfd86_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision
+
+Revision ID: 85065f5bfd86
+Revises: 
+Create Date: 2024-02-03 22:06:01.288711
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85065f5bfd86'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
